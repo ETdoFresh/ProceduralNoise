a4f7fad7adc97ba640a02ac8c0063bbd54abffb7
ETdoFresh <ETdoFresh@gmail.com>
2020-02-15 18:26:34 -0600

    Initial Commit

---
+++ Assets/Demo/Scripts/RandomTiles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace ETdoFresh.PerlinNoise.Demo
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class RandomTiles : MonoBehaviour
+    {
+        public Tilemap tilemap;
+        public int width;
+        public int height;
+
+        private void OnEnable()
+        {
+            if (!tilemap) tilemap = GetComponent<Tilemap>();
+            width = tilemap.size.x;
+            height = tilemap.size.y;
+
+            for(int x = 0; x < tilemap.size.x; x++)
+                for (int y = 0; y < tilemap.size.y; y++)
+                    tilemap.SetColor(new Vector3Int(x, y, 0), RandomColor());
+        }
+
+        public Color RandomColor()
+        {
+            var value = Random.Get();
+            return new Color(value, value, value);
+        }
+    }
+}
---
+++ Assets/Random.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ETdoFresh.PerlinNoise
+{
+    public static class Random
+    {
+        public static float Get()
+        {
+            return Get(0, 1);
+        }
+
+        public static float Get(float min, float max)
+        {
+            return UnityEngine.Random.Range(min, max);
+        }
+    }
+}
