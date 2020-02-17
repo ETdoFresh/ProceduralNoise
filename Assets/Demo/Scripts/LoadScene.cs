83bfa4f67ef49be2ebc185e6b8bc4f5c40fbc535
ETdoFresh <ETdoFresh@gmail.com>
2020-02-16 22:21:28 -0600

    Add LoadScene for Scene Selection Scene

---
+++ Assets/Demo/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Load(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    public void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
